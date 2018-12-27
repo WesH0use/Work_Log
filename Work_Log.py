93ae6491c6e9a41d27c33cad9713e4eddc46ecda
Wesley House <wtreehaus@gmail.com>
2018-12-27 14:26:05 -0500

    Updated variable name errors and typos

--- Work_Log.py
+++ Work_Log.py
@@ -1,177 +1,7 @@
 import datetime
 import csv
+import re
 
-def search():
-    """
-    This function provides search options for the user to use on the work log
-    """
-    while True:
-        search_input = input("""Do you want to search by: \n
-                             a) Exact Date\n
-                             b) Time spent\n
-                             c) Exact Search\n
-                             d) Pattern\n
-                             e) Return to main menu\n""")
-
-        if search_input.lower() == 'a':
-            date_search_valid()
-
-        if search_input.lower() == 'b':
-            while True:
-                try:
-                    user_input = int(input("Please enter time spent (numbers only): "))
-                    break
-                except ValueError:
-                    print("Please enter a valid integer. No decimals or fractions.")
-
-            search_by_time_spent(user_input)
-
-        if search_input.lower() == 'c':
-            string_search = input("Please enter your search: ")
-            search_by_string(string_search)
-
-
-def search_by_string(string_search):
-    """
-    Search the csv file for a matching string provided by the user.
-    """
-    try:
-        with open("log.csv", newline='') as csvfile:
-            fieldnames = ["Date", "Name", "Time", "Notes"]
-            log_reader = csv.DictReader(csvfile, fieldnames=fieldnames)
-            rows = list(log_reader)
-
-            found = False
-            for row in rows:
-                if row["Title"] == string_search or row["Notes"] == string_search:
-                    found = True
-                    print('Date:', row['Date'])
-                    print('Title:', row['Task Name'])
-                    print('Time Spent:', row['Time Spent'])
-                    print('Notes:', row['Notes'])
-                    print('')
-            if found:
-                input("Search result displayed. Please hit enter to return to the main menu")
-
-            if not found:
-                print("Search entry does not exist")
-
-    except FileNotFoundError:
-        print("No entries have been recorded yet")
-        input("Please press enter to return to the main menu")
-
-
-
-def search_by_time_spent(user_input):
-    """
-    Search the csv file for a matching time spent provided by the user.
-    """
-    try:
-        with open("log.csv", newline='') as csvfile:
-            fieldnames = ["Date", "Name", "Time", "Notes"]
-            log_reader = csv.DictReader(csvfile, fieldnames=fieldnames)
-            rows = list(log_reader)
-
-            found = False
-            for row in rows:
-                if row["Time"]==str(user_input):
-                    found = True
-                    print('Date:', row['Date'])
-                    print('Title:', row['Task Name'])
-                    print('Time Spent:', row['Time Spent'])
-                    print('Notes:', row['Notes'])
-                    print('')
-            if found:
-                input("Search result displayed. Please hit enter to return to the main menu")
-
-            if not found:
-                print("Search entry does not exist")
-
-    except FileNotFoundError:
-        print("No entries have been recorded yet")
-        input("Please press enter to return to the main menu")
-
-
-def date_search_valid():
-    """
-    This checks to see if the date provided by the user is valid using datetime.datetime. If it is valid
-    it runs it through a search by date function.
-    """
-
-    while True:
-
-        search_input = input("Please provide a date using the MM/DD/YYYY format ")
-
-        try:
-            datetime.datetime.strptime(search_input, "%m/%d/%Y")
-            search_by_date(search_input)
-            break
-
-        except ValueError:
-            print("Please provide a valid date using the format MM/DD/YYYY")
-
-
-def search_by_date(search_input):
-    """
-    Search the csv file for a matching date provided by the user.
-    """
-    try:
-        with open("log.csv", newline='') as csvfile:
-            fieldnames = ["Date", "Name", "Time", "Notes"]
-            log_reader = csv.DictReader(csvfile, fieldnames=fieldnames)
-            rows = list(log_reader)
-
-            found = False
-            for row in rows:
-                if row["Date"] == search_input:
-                    found = True
-                    print('Date:', row['Date'])
-                    print('Title:', row['Task Name'])
-                    print('Time Spent:', row['Time Spent'])
-                    print('Notes:', row['Notes'])
-                    print('')
-            if found:
-                input("Search result displayed. Please hit enter to return to the main menu")
-
-            if not found:
-                print("Search entry does not exist")
-
-    except FileNotFoundError:
-        print("No entries have been recorded yet")
-        input("Please press enter to return to the main menu")
-
-
-
-def date_add():
-    """
-    This asks the user for the date of the task. It requires the format to be MM/DD/YYYY.
-    """
-
-    while True:
-        date_input = input("Please provide a date for the tasking. Use the format MM/DD/YYYY: ")
-
-        try:
-            datetime.datetime.strptime(date_input, '%m/%d/%Y')
-            return date_input
-
-        except ValueError:
-            print("Please provide a valid date")
-
-
-def add_entry(task_date, task_name, time_spent, notes):
-    """
-    This function takes variables from user input and writes them out to a CSV file. If no file exists
-    one is created. Objects are stored as Dictwriter so that they can be searched later on easily.
-    """
-    with open('log.csv', 'a', newline='') as csvfile:
-        fieldnames = ['Date', 'Name', 'Time Spent', 'Notes']
-        log_writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
-        log_writer.writerow({
-            "Date": "{}".format(task_date),
-            "Name": "{}".format(task_name),
-            "Time": "{}".format(time_spent),
-            "Notes": "{}".format(notes)
-        })
 
 
 def main_menu():
@@ -199,12 +29,245 @@ def main_menu():
 
             add_entry(task_date, task_name, time_spent, notes)
 
-            print("Your entry has been added. Please press enter to return to the main menue")
+            print("Your entry has been added. Please press enter to return to the main menu")
 
         if start_prompt.lower() == 'b':
             search()
             continue
-            # create search function
+
+
+def add_entry(task_date, task_name, time_spent, notes):
+    """
+    This function takes variables from user input and writes them out to a CSV file. If no file exists
+    one is created. Objects are stored as DictWriter so that they can be searched later on easily.
+    """
+    with open('log.csv', 'a', newline='') as csvfile:
+        fieldnames = ['Date', 'Name', 'Time', 'Notes']
+        log_writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
+        log_writer.writerow({
+            "Date": "{}".format(task_date),
+            "Name": "{}".format(task_name),
+            "Time": "{}".format(time_spent),
+            "Notes": "{}".format(notes)
+        })
+
+def date_add():
+    """
+    This asks the user for the date of the task. It requires the format to be MM/DD/YYYY.
+    """
+
+    while True:
+        date_input = input("Please provide a date for the tasking. Use the format MM/DD/YYYY: ")
+
+        try:
+            datetime.datetime.strptime(date_input, '%m/%d/%Y')
+            return date_input
+
+        except ValueError:
+            print("Please provide a valid date")
+
+
+def search():
+    """
+    This function provides search options for the user to use on the work log
+    """
+    while True:
+        search_input = input("""Do you want to search by: \n
+                             a) Exact Date\n
+                             b) Time spent\n
+                             c) Exact Search\n
+                             d) Regex Pattern\n
+                             e) Return to main menu\n""")
+
+        if search_input.lower() == 'a':
+            date_search_valid()
+
+        if search_input.lower() == 'b':
+            while True:
+                try:
+                    user_input = int(input("Please enter time spent (numbers only): "))
+                    break
+                except ValueError:
+                    print("Please enter a valid integer. No decimals or fractions.")
+
+            search_by_time_spent(user_input)
+
+        if search_input.lower() == 'c':
+            string_search = input("Please enter your search: ")
+            search_by_string(string_search)
+
+        if search_input.lower() == 'd':
+            regex_pattern_search = input("Please enter a regex pattern: ")
+            user_regex = (re.compile(r'''
+                %s
+             ''' % user_regex, re.X | re.M))
+            search_by_regex_pattern(regex_pattern_search)
+
+        if search_input.lower() == "e":
+            break
+
+
+# a)
+
+
+def date_search_valid():
+    """
+    This checks to see if the date provided by the user is valid using datetime.datetime. If it is valid
+    it runs it through a search by date function.
+    """
+
+    while True:
+
+        date_search_input = input("Please provide a date using the MM/DD/YYYY format ")
+
+        try:
+            datetime.datetime.strptime(date_search_input, "%m/%d/%Y")
+            search_by_date(date_search_input)
+            break
+
+        except ValueError:
+            print("Please provide a valid date using the format MM/DD/YYYY")
+
+
+def search_by_date(date_search_input):
+    """
+    Search the csv file for a matching date provided by the user.
+    """
+    try:
+        with open("log.csv", newline='') as csvfile:
+            fieldnames = ["Date", "Task Name", "Time", "Notes"]
+            log_reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+            rows = list(log_reader)
+
+            found = False
+            for row in rows:
+                if row["Date"] == date_search_input:
+                    found = True
+                    print('Date:', row['Date'])
+                    print('Title:', row['Task Name'])
+                    print('Time:', row['Time'])
+                    print('Notes:', row['Notes'])
+                    print('')
+            if found:
+                input("Search result displayed. Please hit enter to return to the main menu")
+
+            if not found:
+                print("Search entry does not exist")
+
+    except FileNotFoundError:
+        print("No entries have been recorded yet")
+        input("Please press enter to return to the main menu")
+
+
+# b)
+
+
+def search_by_time_spent(user_input):
+    """
+    Search the csv file for a matching time spent provided by the user.
+    """
+    try:
+        with open("log.csv", newline='') as csvfile:
+            fieldnames = ["Date", "Task Name", "Time", "Notes"]
+            log_reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+            rows = list(log_reader)
+
+            found = False
+            for row in rows:
+                if row["Time"] == str(user_input):
+                    found = True
+                    print('Date:', row['Date'])
+                    print('Title:', row['Task Name'])
+                    print('Time:', row['Time'])
+                    print('Notes:', row['Notes'])
+                    print('')
+            if found:
+                input("Search result displayed. Please hit enter to return to the main menu")
+
+            if not found:
+                print("Search entry does not exist")
+
+    except FileNotFoundError:
+        print("No entries have been recorded yet")
+        input("Please press enter to return to the main menu")
+
+# c
+
+
+def search_by_string(string_search):
+    """
+    Search the csv file for a matching string provided by the user.
+    """
+    try:
+        with open("log.csv", newline='') as csvfile:
+            fieldnames = ["Date", "Task Name", "Time", "Notes"]
+            log_reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+            rows = list(log_reader)
+
+            found = False
+            for row in rows:
+                if row["Title"] == string_search or row["Notes"] == string_search:
+                    found = True
+                    print('Date:', row['Date'])
+                    print('Title:', row['Task Name'])
+                    print('Time:', row['Time'])
+                    print('Notes:', row['Notes'])
+                    print('')
+            if found:
+                input("Search result displayed. Please hit enter to return to the main menu")
+
+            if not found:
+                print("Search entry does not exist")
+
+    except FileNotFoundError:
+        print("No entries have been recorded yet")
+        input("Please press enter to return to the main menu")
+
+
+# d
+
+
+def search_by_regex_pattern(pattern_search):
+    """
+    Search the csv file for a matching pattern provided by the user.
+    """
+    try:
+        with open("log.csv", newline='') as csvfile:
+            fieldnames = ["Date", "Task Name", "Time", "Notes"]
+            log_reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+            rows = list(log_reader)
+
+            found = False
+            for row in rows:
+                if pattern_search.search(str(row)):
+                    found = True
+                    print('Date:', row['Date'])
+                    print('Title:', row['Task Name'])
+                    print('Time:', row['Time'])
+                    print('Notes:', row['Notes'])
+                    print('')
+            if found:
+                input("Search result displayed. Please hit enter to return to the main menu")
+
+            if not found:
+                print("Search entry does not exist")
+
+    except FileNotFoundError:
+        print("No entries have been recorded yet")
+        input("Please press enter to return to the main menu")
+
+
+main_menu()
+
+
+
+
+
+
+
+
+
+
 
 
 
